442c4c66a41b5d165c043bc889184d25a18f0a86
Colin Lyons <colin@tercet.io>
2015-08-20 23:10:27 +0100

    I need to generics this at some point soon.

--- src/Projector/Projecting.php
+++ src/Projector/Projecting.php
@@ -7,7 +7,6 @@ trait Projecting
 
     private function from_camel_case($input)
     {
-
         preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
 
         $ret = $matches[0];
@@ -20,24 +19,22 @@ trait Projecting
 
     private function get_function_name(Event $e)
     {
-
         $reflect = new \ReflectionClass($e);
         $class_name = $reflect->getShortName();
 
         $namespaced_class = get_class($e);
         $words = preg_split('#[\\\.]#', $namespaced_class, -1, PREG_SPLIT_NO_EMPTY);
 
-        return 'when_' . strtolower($words[2]) . '_' . strtolower($words[4]) . '_' . $this->from_camel_case($class_name);
+        return 'when_' . strtolower($words[1]) . '_' . strtolower($words[3]) . '_' . $this->from_camel_case($class_name);
     }
 
     private function mutate(Projectable $item)
     {
-
-        $function = $this->get_function_name($item->event());
-
-        if (!method_exists($this, $function)) {
+        if (!$this->can_apply($item)) {
             return false;
         }
+
+        $function = $this->get_function_name($item->event());
 
         $this->$function($this->projection, $item);
     }
